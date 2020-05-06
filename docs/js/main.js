5e8fd981076182b922fc11d35e897c9df953968f
AlexWong <64052585+otoha-helper@users.noreply.github.com>
2020-05-06 10:24:27 +0800

    Update star 4 text display

--- docs/js/main.js
+++ docs/js/main.js
@@ -2213,7 +2213,10 @@ function genExtarDetal() {
 
         var star4 = $.map(star4Data, function (row) {
             if (row['had4']){
-                var pos = !!(row['pos']) ? " ( " + row['pos'] + " )" : '';
+                var pos = !!(row['pos']) ? " - " + row['pos'] : '';
+                if (!!row['pos']){
+                    return '【'+row['name'] + pos+'】';
+                }
                 return row['name'] + pos;
             }
         });
